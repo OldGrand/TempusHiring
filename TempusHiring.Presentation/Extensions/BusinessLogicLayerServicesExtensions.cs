cefdbfe49e696b613531c143b6f153b8cfb5fd23
Sergey Sipov <37745809+OldGrand@users.noreply.github.com>
2021-02-13 18:46:05 +0300

    Update BusinessLogicLayerServicesExtensions.cs

--- TempusHiring.Presentation/Extensions/BusinessLogicLayerServicesExtensions.cs
+++ TempusHiring.Presentation/Extensions/BusinessLogicLayerServicesExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using TempusHiring.BusinessLogic.Services;
 using TempusHiring.BusinessLogic.Services.Implementation;
+using TempusHiring.BusinessLogic.Services.Implementation.Common;
 using TempusHiring.BusinessLogic.Services.Interfaces;
+using TempusHiring.BusinessLogic.Services.Interfaces.Common;
 
 namespace TempusHiring.Presentation.Extensions
 {
@@ -14,6 +16,16 @@ namespace TempusHiring.Presentation.Extensions
             services.AddScoped<ICatalogService, CatalogService>();
             services.AddScoped<IShoppingCartService, ShoppingCartService>();
             services.AddScoped<IOrderService, OrderService>();
+            services.AddScoped<IBodyMaterialService, BodyMaterialService>();
+            services.AddScoped<IColorService, ColorService>();
+            services.AddScoped<IGlassMaterialService, GlassMaterialService>();
+            services.AddScoped<IManufacturerService, ManufacturerService>();
+            services.AddScoped<IMechanismService, MechanismService>();
+            services.AddScoped<IStrapMaterialService, StrapMaterialService>();
+            services.AddScoped<IStrapService, StrapService>();
+            services.AddScoped<IWristSizeService, WristSizeService>();
+            
+            services.AddScoped(typeof(ICrudService<>), typeof(CrudService<,>));
         }
     }
 }
