8804885419cce8c937c90d555c120f92c814262e
Sergey Sipov <37745809+OldGrand@users.noreply.github.com>
2021-02-26 16:29:11 +0300

    commit

--- TempusHiring.Presentation/Models/ViewModels/Watch/CreateWatchViewModel.cs
+++ TempusHiring.Presentation/Models/ViewModels/Watch/CreateWatchViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using TempusHiring.DataAccess.EntityEnums;
 
 namespace TempusHiring.Presentation.Models.ViewModels.Watch
@@ -30,12 +31,24 @@ namespace TempusHiring.Presentation.Models.ViewModels.Watch
         [Range(0, int.MaxValue)]
         public int SaledCount { get; set; }
 
+
         public Gender Gender { get; set; }
+
+        public SelectList Manufacturers { get; set; }
         public int ManufacturerId { get; set; }
+
+        public SelectList GlassMaterials { get; set; }
         public int GlassMaterialId { get; set; }
+
+        public SelectList Mechanisms { get; set; }
         public int MechanismId { get; set; }
+
+        public SelectList BodyMaterials { get; set; }
         public int BodyMaterialId { get; set; }
+
+        public SelectList Straps { get; set; }
         public int StrapId { get; set; }
+
         public ICollection<PhotoViewModel> Photos { get; set; }
     }
 }
